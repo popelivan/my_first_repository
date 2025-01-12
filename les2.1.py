79af8b6dc148409d2ddbb5711d1a2723fcd0c3fa
popelivan <62383233+popelivan@users.noreply.github.com>
2025-01-12 19:47:43 +0200

    Add files via upload

---
+++ les2.1.py
@@ -0,0 +1,20 @@
+# my_list = [1, 5, 4, 4]
+# list2 = my_list.copy()
+# list2.append(8)
+
+# print(my_list)
+# my_tuple = 1, 2, 5, 6
+# print(type(my_tuple))
+# print(my_list[-1])
+# my_tuple = 1, 2, 5, 6, 7, 9, 10, 11
+# my_tuple_reversed = my_tuple[::-1]
+# print(my_tuple_reversed)
+# result = my_tuple_reversed[::2]
+# print(result)
+a=(34,26,3,45,3,8)
+result = a[::2]
+print(result)  
+
+
+
+
